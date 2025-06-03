17620e5337ffdea1042d1449b1ee244dda638b12
Gavin2 <gavin.furtado12@gmail.com>
2025-06-03 13:13:16 +0530

    first commit

---
+++ backend/src/main/java/com/draftleague/pokemon_showdown_draft_league/PokemonShowdownDraftLeagueApplication.java
@@ -0,0 +1,14 @@
+package com.draftleague.pokemon_showdown_draft_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonShowdownDraftLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonShowdownDraftLeagueApplication.class, args);
+
+	}
+
+}
---
+++ backend/src/test/java/com/draftleague/pokemon_showdown_draft_league/PokemonShowdownDraftLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draftleague.pokemon_showdown_draft_league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonShowdownDraftLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
