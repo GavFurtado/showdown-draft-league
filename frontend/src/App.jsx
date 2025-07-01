93358e826ce864f2604b0ed76793b55222e14d8b
Gavin <gavin.furtado12@gmail.com>
2025-07-02 01:29:44 +0530

    most stupid rebase but whatever

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />}/>
-        <Route path='/bruh' element={<Draftboard />}/>
+        <Route path='/' element={<Login />} />
+        <Route path='/bruh' element={<Draftboard />} />
       </Routes>
     </BrowserRouter>
   )
