33faa1956a0964e00139fb00ec39e1ec2911c681
Ruata Hmar <hvanlalruata511@gmail.com>
2025-07-01 18:27:27 +0530

    navbar v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login"
-
+import Navbar from "./componenets/navbar"
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />}/>
+        <Route path='/' element={<Navbar />}/>
       </Routes>
     </BrowserRouter>
   )
