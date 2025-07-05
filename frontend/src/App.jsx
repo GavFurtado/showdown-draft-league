7d6071881dd6dc19aa1761aff78378e055ba1811
Gavin <gavin.furtado12@gmail.com>
2025-07-05 14:56:27 +0530

    fixed conflicts i think

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login"
+import Navbar from "./componenets/navbar"
 import Dashboard from './pages/dashboard';
 import StandingsTable from './pages/dashboard';
 import Draftboard from "./pages/draftboard"
@@ -8,9 +9,9 @@ import Draftboard from "./pages/draftboard"
 function App() {
   return (
     <BrowserRouter>
-      { /* Add auth to Navbar so that it only shows when user is logged in */ }
+      { /* Add auth to Navbar so that it only shows when user is logged in */}
       <Routes>
-        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/' element={<Login />} />
         <Route path='/draftboard' element={<Draftboard />} />
       </Routes>
@@ -19,3 +20,4 @@ function App() {
 }
 
 export default App
+
--- frontend/src/pages/draftboard.jsx
+++ frontend/src/pages/draftboard.jsx
@@ -2,21 +2,21 @@ import NavBar from "../componenets/navbar"
 import DraftCard from "../componenets/draftCards"
 import pokemonData from "../assets/pokemons.json"
 import Filter from "../componenets/filter"
-import {useState} from "react"
+import { useState } from "react"
 
 
 
-export default function draftboard(){
-    const cards = pokemonData.map(pokemon=>{
-    const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-    const [filters, setFilters] = useState({
-        selectedTypes: [],
-        selectedCost: '',
-        sortByStat: '',
-        sortOrder: 'asc',
-    });
+export default function draftboard() {
+    const cards = pokemonData.map(pokemon => {
+        const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+        const [filters, setFilters] = useState({
+            selectedTypes: [],
+            selectedCost: '',
+            sortByStat: '',
+            sortOrder: 'asc',
+        });
         return <DraftCard key={pokemon.id}
-            name={name} 
+            name={name}
             pic={pokemon.sprites.front_default}
             type={pokemon.types}
             hp={pokemon.stats.hp}
@@ -26,67 +26,86 @@ export default function draftboard(){
             specialAtk={pokemon.stats["special-attack"]}
             specialDef={pokemon.stats["special-defense"]}
             speed={pokemon.stats.speed}
-        
+
         />
     })
     //for filter: stats, types, cost, name
-    return(
+    return (
         <>
-        <div className="h-full bg-[#BFC0C0]  ">
-            <NavBar page="Draftboard"/>
-            
-            <div className="flex flex-row">
-                <div className="flex flex-col w-[70%]">
-                    <div className="flex flex-row m-4 p-8 pb-0 mb-2 justify-between">
-                        {/* search button */}
-                        <div className="relative flex text-black">
-                            <input
-                                type="search"
-                                className="placeholder:text-black relative m-0 block flex-auto rounded border border-solid border-black bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none  dark:autofill:shadow-autofill dark:focus:border-primary"
-                                placeholder="Search"
-                                aria-label="Search"
-                                id="exampleFormControlInput2"
-                                aria-describedby="button-addon2" />
-                            <span
-                                className="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
-                                id="button-addon2">
-                                <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="2"
-                                stroke="black">
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                                </svg>
-                            </span>
-                        </div>
-                        <Filter/>
-                    </div>
-                    <div className="grid grid-cols-5 gap-4 m-4 p-6 mt-0 pt-0 pr-8 overflow-scroll h-screen rounded-2xl">
-                        {cards}
-                    </div>
-                </div>
-            
-                <div className="bg-white shadow-md rounded-md overflow-hidden w-[25%] mx-auto mt-16 ml-2 h-[100%]">
-                    <div className="bg-gray-100 py-2 px-4">
-                        <h2 className="text-l font-semibold text-gray-800">Your Team</h2>
-                    </div>
-                    <ul className="divide-y divide-gray-200">
-                        <li className="flex items-center py-4 px-6">
-                            
-                            <img className="w-12 h-12 object-cover mr-4" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" alt="User avatar"></img>
-                            <div className="flex-1">
-                                <h3 className="text-lg font-medium text-gray-800">BIG MAN BLASTOISE</h3>
-                                {/* <p className="text-gray-600 text-base">1234 points</p> */}
+            <div className="h-full bg-[#BFC0C0]  ">
+                <NavBar page="Draftboard" />
+
+                <div className="flex flex-row">
+                    <div className="flex flex-col w-[70%]">
+                        <div className="flex flex-row m-4 p-8 pb-0 mb-2 justify-between">
+                            {/* search button */}
+                            <div className="relative flex text-black">
+                                <input
+                                    type="search"
+                                    className="placeholder:text-black relative m-0 block flex-auto rounded border border-solid border-black bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none  dark:autofill:shadow-autofill dark:focus:border-primary"
+                                    placeholder="Search"
+                                    aria-label="Search"
+                                    id="exampleFormControlInput2"
+                                    aria-describedby="button-addon2" />
+                                <span
+                                    className="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
+                                    id="button-addon2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="2"
+                                        stroke="black">
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                    </svg>
+                                </span>
                             </div>
-                        </li>   
-                    </ul>
+                            <Filter />
+                        </div>
+                        <div className="grid grid-cols-5 gap-4 m-4 p-6 mt-0 pt-0 pr-8 overflow-scroll h-screen rounded-2xl">
+                            {cards}
+                        </div>
+                    </div>
+
+                    <div className="bg-white shadow-md rounded-md overflow-hidden w-[25%] mx-auto mt-16 ml-2 h-[100%]">
+                        <div className="bg-gray-100 py-2 px-4">
+                            <h2 className="text-l font-semibold text-gray-800">Your Team</h2>
+                        </div>
+                        <ul className="divide-y divide-gray-200">
+                            <li className="flex items-center py-4 px-6">
+
+                                <img className="w-12 h-12 object-cover mr-4" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" alt="User avatar"></img>
+                                <div className="flex-1">
+                                    <h3 className="text-lg font-medium text-gray-800">BIG MAN BLASTOISE</h3>
+                                    {/* <p className="text-gray-600 text-base">1234 points</p> */}
+                                </div>
+                                <Filter />
+                            </div>
+                            <div className="grid grid-cols-5 gap-4 m-4 p-6 mt-0 pt-0 pr-8 overflow-scroll h-screen rounded-2xl">
+                                {cards}
+                            </div>
+                    </div>
+
+                    <div className="bg-white shadow-md rounded-md overflow-hidden w-[25%] mx-auto mt-16 ml-2 h-[100%]">
+                        <div className="bg-gray-100 py-2 px-4">
+                            <h2 className="text-l font-semibold text-gray-800">Your Team</h2>
+                        </div>
+                        <ul className="divide-y divide-gray-200">
+                            <li className="flex items-center py-4 px-6">
+
+                                <img className="w-12 h-12 object-cover mr-4" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" alt="User avatar"></img>
+                                <div className="flex-1">
+                                    <h3 className="text-lg font-medium text-gray-800">BIG MAN BLASTOISE</h3>
+                                    {/* <p className="text-gray-600 text-base">1234 points</p> */}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
