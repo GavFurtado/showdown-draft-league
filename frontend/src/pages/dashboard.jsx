7fca98c50e866b67ebba088c56ebb7c05dacce9b
Ansh Saini <ansh210904@gmail.com>
2025-07-05 18:42:59 +0530

    nothing much
    lil tweak on dashboard

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -6,21 +6,22 @@ import PlayerScedhule from "../componenets/playerScedhule";
 export default function Dashboard(){
   return (
   <div className="container-dashboard">
-    <div className="table-1"> 
-      {/* table-1 for standings */}
-      <StandingsTable />
+      <div className="player-welcome"><h1>Welcome Back, Player.</h1></div>
+      <div className="table-1"> 
+        {/* table-1 for standings */}
+        <StandingsTable />
+      </div>
+      <div className="table-2 ">
+        {/* table 2 for scores */}
+        <Score />
+      </div>
+      <div className="table-3">
+        {/* table 3 for sced */}
+        <PlayerScedhule />
+      </div>
+      <div className="table-4">
+        {/* table 4 for roster */}  
+          <Roster />
+      </div>
     </div>
-    <div className="table-2">
-      {/* table 2 for scores */}
-      <Score />
-    </div>
-     <div className="table-3">
-      {/* table 2 for sced */}
-      <PlayerScedhule />
-    </div>
-     <div className="table-4">
-      {/* table 2 for roster */}
-        <Roster />
-    </div>
-  </div>
 )}
