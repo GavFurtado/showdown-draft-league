8bbd63470a6bc2b892a742e77498a5747612bad6
Gavin <gavin.furtado12@gmail.com>
2025-10-04 20:00:48 +0530

    league pokemon minor change (i lied earlier)

--- new-backend/internal/models/league-pokemon.go
+++ new-backend/internal/models/league-pokemon.go
@@ -9,15 +9,14 @@ import (
 
 // LeaguePokemon are the Pokemon availabile for a particular league
 type LeaguePokemon struct {
-	ID               uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-	LeagueID         uuid.UUID `gorm:"type:uuid;not null;uniqueIndex:idx_league_pokemon_species" json:"league_id"`
-	PokemonSpeciesID int64     `gorm:"type:not null;uniqueIndex:idx_league_pokemon_species" json:"pokemon_species_id"`
-	Cost             *int      `gorm:"not null" json:"cost"`                      // League-specific cost for this Pokemon species
-	IsAvailable      bool      `gorm:"not null;default:true" json:"is_available"` // Can this species be drafted in this league?
-
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID               uuid.UUID      `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	LeagueID         uuid.UUID      `gorm:"type:uuid;not null;uniqueIndex:idx_league_pokemon_species" json:"league_id"`
+	PokemonSpeciesID int64          `gorm:"type:not null;uniqueIndex:idx_league_pokemon_species" json:"pokemon_species_id"`
+	Cost             *int           `gorm:"not null" json:"cost"`                      // League-specific cost for this Pokemon species
+	IsAvailable      bool           `gorm:"not null;default:true" json:"is_available"` // Can this species be drafted in this league?
+	CreatedAt        time.Time      `json:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at"`
+	DeletedAt        gorm.DeletedAt `gorm:"index" json:"-"`
 
 	// Relationships
 	League         League         `gorm:"foreignKey:LeagueID;references:ID"`
